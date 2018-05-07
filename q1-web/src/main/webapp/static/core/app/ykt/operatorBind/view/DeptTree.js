d892792df5fb921ec5f834b4574ca756db3cd332
Tzy <1228500587@qq.com>
2018-05-07 15:17:13 +0800

    bug

--- q1-web/src/main/webapp/static/core/app/ykt/operatorBind/view/DeptTree.js
+++ q1-web/src/main/webapp/static/core/app/ykt/operatorBind/view/DeptTree.js
@@ -1,6 +1,6 @@
-Ext.define("core.ykt.studentmanager.view.DeptTree", {
+Ext.define("core.ykt.operatorBind.view.DeptTree", {
     extend: "core.base.view.BaseTreeGrid",
-    alias: "widget.baseset.studentmanager.depttree",
+    alias: "widget.ykt.operatorBind.depttree",
     dataUrl: comm.get('baseUrl') + "/PtDepartment/getUserRightDeptTree",
     model: factory.ModelFactory.getModelByName("com.yc.q1.pojo.base.pt.DepartmentTree", "checked").modelName,
     al: true,
