d737b41d65ef42e4355b3b91852ff545e99ac695
Tzy <1228500587@qq.com>
2018-05-17 11:57:39 +0800

    系统logo设置

--- q1-web/src/main/webapp/static/core/app/system/versioninfo/controller/OtherController.js
+++ q1-web/src/main/webapp/static/core/app/system/versioninfo/controller/OtherController.js
@@ -15,6 +15,7 @@ Ext.define("core.system.versioninfo.controller.OtherController", {
         "baseformtab[detCode=versioninfo_detail] button[ref=formSave]": {
                 click: function (btn) {
                     this.saveVersionInfo_Tab(btn);
+                    return false;
                 }
             },
 
@@ -44,52 +45,57 @@ Ext.define("core.system.versioninfo.controller.OtherController", {
         //判断当前是保存还是修改操作
         var act = "doUpdateVer";
         if (formObj.isValid()) {
+            formObj.submit({
+                url: comm.get('baseUrl') + "/PtSysParameter" + "/" + act,
+                //params: params,       //表单的参数会自动上传
+                submitEmptyText: false,     //不提交表单为空值的数据
+                waitMsg: '正在提交，请等待...',
+                success: function (fp, action) {
+                    formObj.reset();
+                    self.Info("保存成功!");
 
-            var loading = new Ext.LoadMask(basetab, {
-                msg: '正在提交，请稍等...',
-                removeMask: true// 完成后移除
-            });
-            loading.show();
+                    var grid = basetab.funData.grid; //此tab是否保存有grid参数
+                    var funCode = "versioninfo_main";
+                    var basePanel = grid.up("basepanel[funCode=" + funCode +"]");
+                    var tabPanel=grid.up("tabpanel[xtype=app-main]");
 
-            self.asyncAjax({
-                url: funData.action + "/" + act,
-                params: params,
-                //回调代码必须写在里面
-                success: function (response) {
-                    var data = Ext.decode(Ext.valueFrom(response.responseText, '{}'));
-
-                    if (data.success) {
-
-                        self.msgbox("提交成功!");
-                       
-                        var grid = basetab.funData.grid; //此tab是否保存有grid参数
-                        if (!Ext.isEmpty(grid)) {
-                            var store = grid.getStore();
-                            act=="doAdd"?store.loadPage(1):store.load();                            
-                        }
-                        loading.hide();
+                    self.asyncAjax({
+                        url: comm.get('baseUrl') + "/PtSysParameter/versionInfoList",
+                        params: {
+                            page: 1,
+                            start: 0,
+                            limit: 0,
+                    },
+                        success: function (response) {
+                        var data = Ext.decode(Ext.valueFrom(response.responseText, '{}'));
+                        var roleUserContainer = basePanel.down("container[ref=versionBaseInfo]");
+                        var roleUserContainer2 = basePanel.down("container[ref=versionBaseInfo2]");
+                        data.mainLogoPhoto = comm.get("virtualFileUrl")+"/"+data.mainLogo;
+                        data.smallLogoPhoto = comm.get("virtualFileUrl")+"/"+data.smallLogo;
+                        data.schoolLogoPhoto = comm.get("virtualFileUrl")+"/"+data.schoolLogo;  
+                        roleUserContainer.setData(data);
+                        roleUserContainer2.setData(data);
                         tabPanel.remove(tabItem);
-                     
-                    } else {
-                        self.Error(data.obj);
-                        loading.hide();
-                    }
+                          }
+                    });
+
                 },
-                failure: function(response) {                   
-                    Ext.Msg.alert('请求失败', '错误信息：\n' + response.responseText);
-                    loading.hide();
+                failure: function (form, action) {
+                    if (!Ext.isEmpty(action.result.obj))
+                        self.Info(action.result.obj);
                 }
             });
 
         } else {
+
             var errors = ["前台验证失败，错误信息："];
             formObj.getFields().each(function (f) {
                 if (!f.isValid()) {
-                    errors.push("<font color=red>" + f.fieldLabel + "</font>：" + f.getErrors().join(","));
+                    errors.push("<font color=red>" + f.fieldLabel + "</font>:" + f.getErrors().join(","));
                 }
             });
             self.msgbox(errors.join("<br/>"));
         }
-    },
+    },  
    
 });
